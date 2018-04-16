edcaab1a4f5d07a43f78e7b4e0af6eb9335cb1b5
PabloHdez <pablohernandezaguado@gmail.com>
2018-04-16 11:01:07 +0200

    JunctionsS

--- src/main/java/es/ucm/fdi/model/SimObj/RobinJunction.java
+++ src/main/java/es/ucm/fdi/model/SimObj/RobinJunction.java
@@ -26,10 +26,15 @@ public class RobinJunction extends Junction {
     int elapsedTime;
 
     /**
-     * Booleano que informa si en un cruce el semáforo ha estado abierto
-     * pero no había ningún coche que pudiese cruzar.
+     * 
      */
     boolean uselessGreen;
+
+    /**
+     * Booleano que informa si en un cruce el semáforo ha estado abierto
+     * y cada vez ha cruzado un coche.
+     */
+    boolean usefulGreen;
 
     public RobinJunction(String identifier, int minTime, int maxTime) {
         super(identifier);
@@ -57,47 +62,53 @@ public class RobinJunction extends Junction {
 			catch (SimulationException e) {
 				System.err.println( e.getMessage() );
 			}
-		}
-
-		// * //
-		// Se actualiza el indicador resp. de la carretera.
-		greenRoad.setLight(false);
+        }
     }
 
     @Override
     public void lightAdvance() {
-        if (light != -1) {
-            Road usedRoad = getIncomingRoads().get(light)
-            int roadTimeLapse = timeLapses.get(usedRoad);
-    
-            // El semáforo ha agotado su tiempo.
-            if ( roadTimeLapse == elapsedTime ) {
-                // 1 // Se calcula la nueva duración del semáforo.
-                int newTimeLapse = 0;
-    
-                if (uselessGreen) {
-                    newTimeLapse = max(roadTimeLapse - 1, minLightTime);
-                }
-                else {
-                    newTimeLapse = min(roadTimeLapse + 1, maxLightTime);
-                }
-    
-                // Nueva duración del semáforo para la carretera.
-                timeLapses.put(usedRoad, newTimeLapse);
-                
-                // 2 // Se pone en verde el semáforo siguiente.
-                // Número de carreteras entrantes en el cruce.
-                int numIncomingRoads = incomingRoads.size();
-                
-                // Avanza en 1 el semáforo circular.
-                light = (light + 1) % numIncomingRoads;
-    
-                // El semáforo de la carretera se pone verde.
-                incomingRoads.get(light).setLight(true);
-            }
-        }
+        Road usedRoad = getIncomingRoads().get(light)
+        int roadTimeLapse = timeLapses.get(usedRoad);
 
-        
+        // El semáforo ha agotado su tiempo.
+        if ( roadTimeLapse == elapsedTime ) {
+            // * //
+            // Se actualiza el indicador resp. de la carretera.
+            greenRoad.setLight(false);
+
+            // 1 // 
+            // Se calcula la nueva duración del semáforo.
+            int newTimeLapse = 0;
+
+            if (uselessGreen) {
+                newTimeLapse = max(roadTimeLapse - 1, minLightTime);
+            } 
+            else if (usefulGreen) {
+                newTimeLapse = min(roadTimeLapse + 1, maxLightTime);
+            }
+            else {
+                newTimeLapse = roadTimeLapse;
+            }
+
+            // Nueva duración del semáforo para la carretera.
+            timeLapses.put(usedRoad, newTimeLapse);
+            
+
+            // 2 // 
+            // Se pone en verde el semáforo siguiente.
+            // Número de carreteras entrantes en el cruce.
+            int numIncomingRoads = incomingRoads.size();
+            
+            // Avanza en 1 el semáforo circular.
+            light = (light + 1) % numIncomingRoads;
+
+            // El semáforo de la carretera se pone verde.
+            incomingRoads.get(light).setLight(true);
+
+            // 3 //
+            // Se resetea elapsedTime
+            elapsedTime = 0;
+        }        
     }
 
 
